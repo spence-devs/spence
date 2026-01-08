664cb908f3770338c633a4ca4e068723033e7cbf
Waris <vaxeradev@gmail.com>
2026-01-08 19:48:46 +0530

    Create __init__.py

---
+++ client/__init__.py
@@ -0,0 +1,29 @@
+from client.__version__ import __version__
+from client.api.node import Node
+from client.api.player import Player
+from client.api.track import Track
+from client.api.queue import Queue
+from client.errors import (
+    SpenceError,
+    ResolverError,
+    UnsupportedPlatform,
+    TrackNotFound,
+    PlayerError,
+    InvalidState,
+    DecodeError,
+)
+
+__all__ = [
+    "__version__",
+    "Node",
+    "Player",
+    "Track",
+    "Queue",
+    "SpenceError",
+    "ResolverError",
+    "UnsupportedPlatform",
+    "TrackNotFound",
+    "PlayerError",
+    "InvalidState",
+    "DecodeError",
+]
